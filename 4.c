dacf57debb11de643502ff80b0a5ff0794ac9543
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2023-12-12 11:04:49 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -24,50 +24,50 @@ The sum of a row 0: 10
 Enter column number: 2
 Elements of column 2: 1, 4, 6
 The sum of column 2: 11*/
-
 #include<stdio.h>
-
 void main(){
 
-int a[100][100];
-int i,j,n,sum=0,row,col;
+	int large,n,r,c,i,j;
+	int x,y;
+	int sum=0,add=0;
+	
+	
+	printf("enter the size of row:");
+	scanf("%d",&r);
+	printf("enter the size of coln:");
+	scanf("%d",&c);
 
-
-
-
-printf("Entr array size = ");
-scanf("%d",&n);
-
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-printf("Enter count [%d][%d]",i,j);
-scanf("%d",&a[i][j]);
-}
-printf("\n");
-}
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-printf("%d",a[i][j]);
-}
-printf("\n");
+	int a[r][c];
+	for(i=0;i<r;i++){
+	for(j=0;j<c;j++){
+		printf("enter a[%d][%d] : ",i,j);
+		scanf("%d",&a[i][j]);
+	}
 }
 
-    printf("Entr row size = ");
-scanf("%d",&row);
-
-printf("Entr col size = ");
-scanf("%d",&col);
-
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-
-if(i==row || j==col){
-sum = sum +a[i][j];
-
+	printf("\nenter row number:");
+	scanf("%d",&x);
+		for(i=0;i<r;i++){
+			for(j=0;j<c;j++){
+				if(i==x){
+					printf("%d ",a[x][j]);
+					sum+=a[x][j];
+		}
+	}
 }
-}
-printf("\n");
-}
-printf("%d",sum);
+
+	printf("\nsum of the row: %d",sum);
+	printf("\nenter cln number: ");
+	scanf("%d",&y);
+
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			if(j==y){
+			printf("%d ",a[i][y]);
+			add+=a[i][y];
+			}
+		}
+	}
+	printf("\nsum of the col.. %d",add);
 }
 
